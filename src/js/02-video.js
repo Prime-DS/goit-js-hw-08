3c2fcc86d4c224c53ad5104d8cc63b5a5c01af92
Prime-DS <prime.up17@gmail.com>
2022-07-25 23:10:47 +0300

    add task 1 and 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+const currentTime = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(currentTime);
+
+function timeUpdate(time){
+    localStorage.setItem("videoplayer-current-time", time.seconds)
+    console.log(time.seconds)
+}
